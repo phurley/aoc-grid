2dde317d4efd6cfe5a3adac7d55a10189ef7e768
Patrick Hurley <phurley@invoca.com>
2023-12-03 20:37:18 -0500

    Improve tests

--- spec/aoc/cursor_spec.rb
+++ spec/aoc/cursor_spec.rb
@@ -32,5 +32,23 @@ RSpec.describe Aoc::Cursor do
       hash[cursor2] = 2
       expect(hash.size).to eq(1)
     end
+
+    it "can be moved" do
+      cursor2 = cursor.move(:down_right)
+      expect(cursor2).to eq(grid.cursor(1, 1))
+      expect(cursor2).not_to eq(cursor)
+      expect(cursor2.hash).not_to eq(cursor.hash)
+      expect(cursor2.neighbors).to include(grid.cursor(0, 0))
+      expect(cursor2.neighbors.size).to eq(8)
+    end
+
+    it "can be move with wrapping" do
+      cursor2 = cursor.move(:left, wrap: true)
+      expect(cursor2).to eq(grid.cursor(9, 0))
+    end
+
+    it "can prevent illegal moves" do
+      expect { cursor.move(:left, wrap: false) }.to raise_error(Aoc::Error)
+    end
   end
 end
